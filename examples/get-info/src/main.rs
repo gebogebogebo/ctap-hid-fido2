fdb848948751f679f29e634c1616b87f7fb27f65
gebo <35388172+gebogebogebo@users.noreply.github.com>
2020-09-21 19:22:56 +0900

    update

--- examples/get-info/src/main.rs
+++ examples/get-info/src/main.rs
@@ -1,15 +1,14 @@
 extern crate ctap_hid_fido2;
 
-// cargo run --example getinfo
-
 fn main() {
-    println!("getinfo - start");
+    let devs = ctap_hid_fido2::get_hid_devices();
+    for dev in devs {
+        println!("vid=0x{:04x} , pid=0x{:04x}", dev.vid, dev.pid);
+    }
 
     let result = ctap_hid_fido2::get_info().unwrap();
 
     for (key, value) in result {
         println!("{} / {}", key, value);
     }
-
-    println!("getinfo - end");
 }
