259c683e05941bf90fe3a369f928b3813dc2b57a
gebo <35388172+gebogebogebo@users.noreply.github.com>
2020-09-26 13:56:24 +0900

    update

--- src/make_credential_with_pin_non_rk_result.rs
+++ src/make_credential_with_pin_non_rk_result.rs
@@ -1,4 +1,8 @@
+/*!
+Result of make_credential_with_pin_non_rk()
+*/
 
+/// Result of make_credential_with_pin_non_rk()
 #[derive(Debug, Default)]
 pub struct MakeCredentialWithPinNonRkResult {
     pub credential_id: Vec<u8>,    
