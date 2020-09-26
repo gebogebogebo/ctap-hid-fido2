398285447eabe16badf1a4074a9db04719c6326c
gebo <35388172+gebogebogebo@users.noreply.github.com>
2020-09-27 08:34:47 +0900

    update

--- examples/get-info/src/main.rs
+++ examples/get-info/src/main.rs
@@ -5,14 +5,20 @@ fn main() {
 
     println!("get_hid_devices()");
     let devs = ctap_hid_fido2::get_hid_devices();
-    for (info,dev) in devs {
-        println!("- vid=0x{:04x} , pid=0x{:04x} , info={:?}", dev.vid, dev.pid,info);
+    for (info, dev) in devs {
+        println!(
+            "- vid=0x{:04x} , pid=0x{:04x} , info={:?}",
+            dev.vid, dev.pid, info
+        );
     }
 
     println!("get_fidokey_devices()");
     let devs = ctap_hid_fido2::get_fidokey_devices();
-    for (info,dev) in devs {
-        println!("- vid=0x{:04x} , pid=0x{:04x} , info={:?}", dev.vid, dev.pid,info);
+    for (info, dev) in devs {
+        println!(
+            "- vid=0x{:04x} , pid=0x{:04x} , info={:?}",
+            dev.vid, dev.pid, info
+        );
     }
 
     println!("get_info()");
@@ -21,22 +27,22 @@ fn main() {
         Err(error) => {
             println!("error: {:?}", error);
             return;
-        },
-    };    
+        }
+    };
     for (key, value) in infos {
         println!("- {} / {}", key, value);
     }
 
     println!("get_pin_retries()");
-    let retry = match ctap_hid_fido2::get_pin_retries(&ctap_hid_fido2::HidParam::get_default_params()) {
-        Ok(result) => result,
-        Err(error) => {
-            println!("error: {:?}", error);
-            return;
-        },
-    };    
+    let retry =
+        match ctap_hid_fido2::get_pin_retries(&ctap_hid_fido2::HidParam::get_default_params()) {
+            Ok(result) => result,
+            Err(error) => {
+                println!("error: {:?}", error);
+                return;
+            }
+        };
     println!("- pin retry = {}", retry);
 
     println!("----- get-info end -----");
-
 }
