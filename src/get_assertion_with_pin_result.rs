b72a961af1c0486e273d4c20cba40dec705e3c55
gebo <35388172+gebogebogebo@users.noreply.github.com>
2020-09-26 13:56:24 +0900

    update

--- src/get_assertion_with_pin_result.rs
+++ src/get_assertion_with_pin_result.rs
@@ -1,4 +1,8 @@
+/*!
+Result of get_assertion_with_pin()
+*/
 
+/// Result of get_assertion_with_pin()
 #[derive(Debug, Default)]
 pub struct GetAssertionWithPinResult {
     pub number_of_credentials: i32,
