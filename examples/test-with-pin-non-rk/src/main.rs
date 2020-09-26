39bbfe0e7403c80136d710d867aa69ad226f85a7
gebo <35388172+gebogebogebo@users.noreply.github.com>
2020-09-26 11:06:08 +0900

    update

--- examples/test-with-pin-non-rk/src/main.rs
+++ examples/test-with-pin-non-rk/src/main.rs
@@ -3,6 +3,7 @@ use ctap_hid_fido2::util;
 
 fn main() {
     println!("----- test-with-pin-non-rk start -----");
+    println!("");
     
     // parameter
     let rpid = "test.com";
@@ -14,19 +15,32 @@ fn main() {
     let hid_params = ctap_hid_fido2::HidParam::get_default_params();
 
     println!("- make_credential_with_pin_non_rk");
-    let cre_id = match ctap_hid_fido2::make_credential_with_pin_non_rk(&hid_params,rpid,&challenge,pin){
-        Ok(n) => n,
+    let result = match ctap_hid_fido2::make_credential_with_pin_non_rk(&hid_params,rpid,&challenge,pin){
+        Ok(result) => result,
         Err(err) => {
             println!("{:?}",err);
             return;
         }
     };
 
-    println!("credential_id({:02})  = {:?}", cre_id.len(),util::to_hex_str(&cre_id));
+    println!("Success!!");
+    println!("credential_id({:02})  = {:?}", result.credential_id.len(),util::to_hex_str(&result.credential_id));
     println!("");
 
+    let cre_id = result.credential_id.to_vec();
+
     println!("- get_assertion_with_pin");
-    ctap_hid_fido2::get_assertion_with_pin(&hid_params,rpid,&challenge,&cre_id,pin);
+    //ctap_hid_fido2::get_assertion_with_pin(&hid_params,rpid,&challenge,&cre_id,pin);
+    let result = match ctap_hid_fido2::get_assertion_with_pin(&hid_params,rpid,&challenge,&cre_id,pin){
+        Ok(result) => result,
+        Err(err) => {
+            println!("{:?}",err);
+            return;
+        }
+    };
+
+    println!("Success!!");
+    println!("number_of_credentials = {:?}",result.number_of_credentials);
     println!("");
 
     println!("----- test-with-pin-non-rk end -----");
---
+++ src/make_credential_with_pin_non_rk_result.rs
@@ -0,0 +1,5 @@
+
+#[derive(Debug, Default)]
+pub struct MakeCredentialWithPinNonRkResult {
+    pub credential_id: Vec<u8>,    
+}
