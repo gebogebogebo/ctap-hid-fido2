2ae4bf167a29762926b4ea66b973636d471922d4
gebo <35388172+gebogebogebo@users.noreply.github.com>
2020-10-04 19:34:31 +0900

    update

--- src/verifier.rs
+++ src/verifier.rs
@@ -1,15 +1,54 @@
 use crate::make_credential_params;
 use crate::util;
 use ring::digest;
+use x509_parser::parse_x509_der;
 
 pub fn verify_attestation(
     rpid: &str,
     challenge: &[u8],
-    attestatin: &make_credential_params::Attestation,
+    attestation: &make_credential_params::Attestation,
 ) {
-    if verify_rpid(rpid, &attestatin.rpid_hash) == false {
+    if verify_rpid(rpid, &attestation.rpid_hash) == false {
         return;
     }
+
+    let public_key = {
+        let res = parse_x509_der(&attestation.attstmt_x5c[0]);
+        let cert = {
+            match res {
+                Ok((rem, cert)) => {
+                    assert!(rem.is_empty());
+                    //
+                    assert_eq!(cert.tbs_certificate.version, 2);
+                    cert
+                },
+                _ => panic!("x509 parsing failed: {:?}", res),
+            }
+        };
+        cert.tbs_certificate.subject_pki.subject_public_key
+    };
+
+
+    let a = 0;
+    /*
+    var result = new Result();
+    var cert = DerConverter.ToPemCertificate(att.AttStmtX5c);
+    var publicKeyforVerify = CryptoBC.GetPublicKeyPEMfromCert(cert);
+    if (!string.IsNullOrEmpty(publicKeyforVerify)) {
+        result.IsSuccess = VerifyPublicKey(publicKeyforVerify, challenge, att.AuthData, att.AttStmtSig);
+    }
+
+    // Verifyの結果によらず
+    {
+        var decAuthdata = new DecodedAuthData();
+        decAuthdata.Decode(att.AuthData);
+        result.CredentialID = decAuthdata.CredentialId;
+        result.PublicKeyPem = decAuthdata.PublicKeyPem;
+    }
+
+    return result;
+    */
+
 }
 
 fn verify_rpid(rpid: &str, rpid_hash: &[u8]) -> bool {
@@ -29,10 +68,3 @@ fn verify_rpid(rpid: &str, rpid_hash: &[u8]) -> bool {
     }
 }
 
-/*
-public Result Verify(string rpid,byte[] challenge, Attestation att)
-        {
-            if (VerifyRpId(rpid, att.RpIdHash) == false) return new Result();
-            return (Verify(challenge, att));
-        }
-*/
