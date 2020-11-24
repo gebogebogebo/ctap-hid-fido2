5e6a74351ec839d741154e2aa41250e6539652e8
gebo <35388172+gebogebogebo@users.noreply.github.com>
2020-11-24 18:49:21 +0900

    update

--- examples/nitro/src/main.rs
+++ examples/nitro/src/main.rs
@@ -1,26 +1,25 @@
 fn main() {
     println!("----- Nitrokey GETVERSION start -----");
     // get 4byte payload "2001" -> ver 2.0.0.1
-    match ctap_hid_fido2::nitrokey::get_version(&ctap_hid_fido2::HidParam::get_default_params()){
-        Ok(version) => println!("version = {}",version),
-        Err(err) => println!("version = {}",err),
+    match ctap_hid_fido2::nitrokey::get_version(&ctap_hid_fido2::HidParam::get_default_params()) {
+        Ok(version) => println!("version = {}", version),
+        Err(err) => println!("version = {}", err),
     };
     println!("----- Nitrokey GETVERSION end -----");
 
     println!("----- Nitrokey GETSTATUS start -----");
     // get 8byte payload
-    match ctap_hid_fido2::nitrokey::get_status(&ctap_hid_fido2::HidParam::get_default_params()){
+    match ctap_hid_fido2::nitrokey::get_status(&ctap_hid_fido2::HidParam::get_default_params()) {
         Ok(status) => status.print("status"),
-        Err(err) => println!("status = {}",err),
+        Err(err) => println!("status = {}", err),
     };
     println!("----- Nitrokey GETSTATUS end -----");
 
     println!("----- Nitrokey GETRNG start -----");
     // get ｘ byte rundom data
-    match ctap_hid_fido2::nitrokey::get_rng(&ctap_hid_fido2::HidParam::get_default_params(),8){
-        Ok(rng) => println!("rng = {}",rng),
-        Err(err) => println!("rng = {}",err),
+    match ctap_hid_fido2::nitrokey::get_rng(&ctap_hid_fido2::HidParam::get_default_params(), 8) {
+        Ok(rng) => println!("rng = {}", rng),
+        Err(err) => println!("rng = {}", err),
     };
     println!("----- Nitrokey GETRNG end -----");
-    
 }
