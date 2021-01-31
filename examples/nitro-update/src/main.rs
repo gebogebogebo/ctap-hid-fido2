8262a8d6cec65ed7ed00e2f1c807625c568ac64c
gebo <35388172+gebogebogebo@users.noreply.github.com>
2021-01-31 10:09:20 +0900

    enterboot

---
+++ examples/nitro-update/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+
+    // ブートローダーモードに遷移する
+    // キーをタッチしてグリーンのランプが点灯した状態で実行すると成功しやすい
+    // 紫のランプ高速点滅状態になれば成功
+    println!("----- Nitrokey ENTERBOOT start -----");
+    match ctap_hid_fido2::nitrokey::enter_boot(&ctap_hid_fido2::HidParam::get_default_params()) {
+        Ok(_) => println!("enter boot Ok"),
+        Err(err) => println!("enter boot Error = {}", err),
+    };
+    println!("----- Nitrokey ENTERBOOT end -----");
+
+}
--- examples/nitro/src/main.rs
+++ examples/nitro/src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
+
     println!("----- Nitrokey GETVERSION start -----");
     // get 4byte payload "2001" -> ver 2.0.0.1
     match ctap_hid_fido2::nitrokey::get_version(&ctap_hid_fido2::HidParam::get_default_params()) {
