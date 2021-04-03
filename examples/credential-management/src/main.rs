15138798022d7c5104227bbb5b3f719195fd999d
gebo <gebogebogeboge@gmail.com>
2021-04-03 17:19:59 +0900

    update

--- examples/credential-management/src/main.rs
+++ examples/credential-management/src/main.rs
@@ -1,12 +1,61 @@
 use ctap_hid_fido2;
 use ctap_hid_fido2::util;
 
-fn main() {
-    ctap_hid_fido2::hello();
+fn metadata(){
+    println!("credential_management_get_creds_metadata()");
+    match ctap_hid_fido2::credential_management_get_creds_metadata(
+        &ctap_hid_fido2::HidParam::get_default_params(),
+        Some("1234"),
+    ) {
+        Ok(result) => {
+            result.print("- creds metadata");
+        }
+        Err(error) => {
+            println!("- creds metadata error: {:?}", error);
+        }
+    };
+}
 
-    println!("----- credential-management start -----");
+fn rps(){
+    println!("credential_management_enumerate_rps()");
+    match ctap_hid_fido2::credential_management_enumerate_rps(
+        &ctap_hid_fido2::HidParam::get_default_params(),
+        Some("1234"),
+    ) {
+        Ok(results) => {
+            for data in results {
+                data.print("- rps");
+            }
+        }
+        Err(error) => {
+            println!("- enumerate rps error: {:?}", error);
+        }
+    };
+}
 
-    // PEND
+fn credentials(){
+    println!("credential_management_enumerate_credentials()");
+    let rpid_hash: Vec<u8> = util::to_str_hex(
+//        "0BDF390F1237B556DB51AF378D5795D5531385CCECDB4499D6BAFBD8918460CA".to_string(),
+        "8C5D729B193185CD17AC242C85E6BD23D3990ABB1C65336559524882A6EACA33".to_string(),
+    );
+    match ctap_hid_fido2::credential_management_enumerate_credentials(
+        &ctap_hid_fido2::HidParam::get_default_params(),
+        Some("1234"),
+        rpid_hash,
+    ) {
+        Ok(results) => {
+            for data in results {
+                data.print("- credentials");
+            }
+        }
+        Err(error) => {
+            println!("- enumerate credentials error: {:?}", error);
+        }
+    };
+}
+
+fn delete(){
     println!("credential_management_delete_credential()");
     let cre_id: Vec<u8> = util::to_str_hex(
         "01BF8A2F9DC667CB3DE31DBB155CC3824BBD57910E7FE4D0DB86FF36DE0CDC1E00258C5D729B193185CD17AC242C85E6BD23D3990ABB1C65336559524882A6EACA3302020000".to_string(),
@@ -27,55 +76,18 @@ fn main() {
             println!("- credential_management_delete_credential error: {:?}", error);
         }
     };
+}
 
-    
-    println!("credential_management_enumerate_credentials()");
-    let rpid_hash: Vec<u8> = util::to_str_hex(
-//        "0BDF390F1237B556DB51AF378D5795D5531385CCECDB4499D6BAFBD8918460CA".to_string(),
-        "8C5D729B193185CD17AC242C85E6BD23D3990ABB1C65336559524882A6EACA33".to_string(),
-    );
-    match ctap_hid_fido2::credential_management_enumerate_credentials(
-        &ctap_hid_fido2::HidParam::get_default_params(),
-        Some("1234"),
-        rpid_hash,
-    ) {
-        Ok(results) => {
-            for data in results {
-                data.print("- credentials");
-            }
-        }
-        Err(error) => {
-            println!("- enumerate credentials error: {:?}", error);
-        }
-    };
+fn main() {
+    ctap_hid_fido2::hello();
 
-    println!("credential_management_get_creds_metadata()");
-    match ctap_hid_fido2::credential_management_get_creds_metadata(
-        &ctap_hid_fido2::HidParam::get_default_params(),
-        Some("1234"),
-    ) {
-        Ok(result) => {
-            result.print("- creds metadata");
-        }
-        Err(error) => {
-            println!("- creds metadata error: {:?}", error);
-        }
-    };
-    println!("credential_management_enumerate_rps()");
-    match ctap_hid_fido2::credential_management_enumerate_rps(
-        &ctap_hid_fido2::HidParam::get_default_params(),
-        Some("1234"),
-    ) {
-        Ok(results) => {
-            for data in results {
-                data.print("- rps");
-            }
-        }
-        Err(error) => {
-            println!("- enumerate rps error: {:?}", error);
-        }
-    };
-    // PEND
+    println!("----- credential-management start -----");
+    metadata();
+    rps();
+    credentials();
+    delete();
+    println!("----- credential-management end -----");
+
 
     /* Test for CTAP 2.1
     match ctap_hid_fido2::config(&ctap_hid_fido2::HidParam::get_default_params()) {
@@ -97,5 +109,4 @@ fn main() {
     };
     */
 
-    println!("----- credential-management end -----");
 }
