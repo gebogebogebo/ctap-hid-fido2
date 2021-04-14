062641966c3bcb5686ab3593d1667a8c209002a6
gebo <gebogebogeboge@gmail.com>
2021-04-14 21:36:39 +0900

    update

--- src/ctapdef.rs
+++ src/ctapdef.rs
@@ -5,6 +5,9 @@ pub const AUTHENTICATOR_GET_ASSERTION: u8 = 0x02;
 pub const AUTHENTICATOR_GET_INFO: u8 = 0x04;
 pub const AUTHENTICATOR_CLIENT_PIN: u8 = 0x06;
 // CTAP 2.1
-pub const AUTHENTICATOR_CREDENTIAL_MANAGEMENT: u8 = 0x0A;
+// 6.8. authenticatorCredentialManagement (0x0A)
+//pub const AUTHENTICATOR_CREDENTIAL_MANAGEMENT: u8 = 0x0A;
+// 6.13. Prototype authenticatorCredentialManagement (0x41) (For backwards compatibility with "FIDO_2_1_PRE" )
+pub const AUTHENTICATOR_CREDENTIAL_MANAGEMENT: u8 = 0x41;
 pub const AUTHENTICATOR_SELECTION: u8 = 0x0B;
 pub const AUTHENTICATOR_CONFIG: u8 = 0x0D;
