5b4953d58ecaf3186b3e06fd43fa8a6f4b7aaa3b
gebo <gebogebogeboge@gmail.com>
2021-04-22 08:02:24 +0900

    update

--- src/bio_enrollment_response.rs
+++ src/bio_enrollment_response.rs
@@ -12,6 +12,11 @@ pub(crate) fn parse_cbor(bytes: &[u8]) -> Result<BioEnrollmentData, String> {
                 0x01 => data.modality = util::cbor_value_to_num(val)?,
                 0x02 => data.fingerprint_kind = util::cbor_value_to_num(val)?,
                 0x03 => data.max_capture_samples_required_for_enroll = util::cbor_value_to_num(val)?,
+                0x07 => {
+                    // PEND
+                    // templateInfos (0x07)	CBOR ARRAY	
+
+                },
                 0x08 => data.max_template_friendly_name = util::cbor_value_to_num(val)?,
                 _ => println!("parse_cbor_member - unknown info {:?}", member),
             }
