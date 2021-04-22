4d6f3ed5186790619343453b8e8921462f60e326
gebo <gebogebogeboge@gmail.com>
2021-04-23 08:27:51 +0900

    update

--- src/bio_enrollment_response.rs
+++ src/bio_enrollment_response.rs
@@ -1,7 +1,7 @@
 #[allow(unused_imports)]
 use crate::util;
 use serde_cbor::Value;
-use crate::bio_enrollment_params::BioEnrollmentData;
+use crate::bio_enrollment_params::{BioEnrollmentData,TemplateInfo};
 
 pub(crate) fn parse_cbor(bytes: &[u8]) -> Result<BioEnrollmentData, String> {
     let mut data = BioEnrollmentData::default();
@@ -13,9 +13,14 @@ pub(crate) fn parse_cbor(bytes: &[u8]) -> Result<BioEnrollmentData, String> {
                 0x02 => data.fingerprint_kind = util::cbor_value_to_num(val)?,
                 0x03 => data.max_capture_samples_required_for_enroll = util::cbor_value_to_num(val)?,
                 0x07 => {
-                    // PEND
-                    // templateInfos (0x07)	CBOR ARRAY	
-
+                    if let Value::Array(xs) = val {
+                        for x in xs {
+                            let mut template = TemplateInfo::default();
+                            template.template_id = util::cbor_get_bytes_from_map(x,"1")?;
+                            template.template_friendly_name = util::cbor_get_string_from_map(x, "2")?;
+                            data.template_infos.push(template);
+                        }
+                    }
                 },
                 0x08 => data.max_template_friendly_name = util::cbor_value_to_num(val)?,
                 _ => println!("parse_cbor_member - unknown info {:?}", member),
