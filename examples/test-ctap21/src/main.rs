dff8f5c0a3619112500817c1f54ffcd860099972
gebo <gebogebogeboge@gmail.com>
2021-04-29 07:53:42 +0900

    update

--- examples/test-ctap21/src/main.rs
+++ examples/test-ctap21/src/main.rs
@@ -1,9 +1,9 @@
 use ctap_hid_fido2;
 
+use ctap_hid_fido2::bio_enrollment_params::TemplateInfo;
 #[allow(unused_imports)]
 use ctap_hid_fido2::util;
-use ctap_hid_fido2::bio_enrollment_params::TemplateInfo;
-use ctap_hid_fido2::{HidParam,InfoParam,InfoOption};
+use ctap_hid_fido2::{HidParam, InfoOption, InfoParam};
 
 extern crate clap;
 use clap::{App, Arg, SubCommand};
@@ -28,77 +28,89 @@ fn main() {
                 .short("i")
                 .long("info"),
         )
-        .subcommand(SubCommand::with_name("bio_enrollment")
-            .about("authenticatorBioEnrollment (0x09)")
-            .arg(
-                Arg::with_name("info")
-                    .help("Get fingerprint sensor info")
-                    .short("i")
-                    .long("info"),
-            )
-            .arg(
-                Arg::with_name("enumerate")
-                    .help("Enumerate enrollments")
-                    .short("e")
-                    .long("enumerate"),
-            )
-            .arg(
-                Arg::with_name("enroll")
-                    .help("Enrolling fingerprint")
-                    .short("n")
-                    .long("enroll"),
-            )
-            .arg(
-                Arg::with_name("rename")
-                    .help("Rename/Set FriendlyName")
-                    .short("r")
-                    .long("rename")
-                    .takes_value(true)
-                    .value_name("templateId")
-                    .value_name("templateFriendlyName"),
-            )
-            .arg(
-                Arg::with_name("delete")
-                    .help("Delete enrollment")
-                    .short("d")
-                    .long("delete")
-                    .takes_value(true)
-                    .value_name("templateId"),
-            )
+        .subcommand(
+            SubCommand::with_name("bio_enrollment")
+                .about("authenticatorBioEnrollment (0x09)")
+                .arg(
+                    Arg::with_name("info")
+                        .help("Get fingerprint sensor info")
+                        .short("i")
+                        .long("info"),
+                )
+                .arg(
+                    Arg::with_name("enumerate")
+                        .help("Enumerate enrollments")
+                        .short("e")
+                        .long("enumerate"),
+                )
+                .arg(
+                    Arg::with_name("enroll")
+                        .help("Enrolling fingerprint")
+                        .short("n")
+                        .long("enroll"),
+                )
+                .arg(
+                    Arg::with_name("rename")
+                        .help("Rename/Set FriendlyName")
+                        .short("r")
+                        .long("rename")
+                        .takes_value(true)
+                        .value_name("templateId")
+                        .value_name("templateFriendlyName"),
+                )
+                .arg(
+                    Arg::with_name("delete")
+                        .help("Delete enrollment")
+                        .short("d")
+                        .long("delete")
+                        .takes_value(true)
+                        .value_name("templateId"),
+                ),
         );
 
     // Parse arguments
     let matches = app.get_matches();
-    
+
     // Start
     ctap_hid_fido2::hello();
 
-    match ctap_hid_fido2::enable_info_param(&HidParam::get_default_params(),InfoParam::VersionsFIDO21PRE) {
+    match ctap_hid_fido2::enable_info_param(
+        &HidParam::get_default_params(),
+        InfoParam::VersionsFIDO21PRE,
+    ) {
         Ok(result) => println!("FIDO 2.1 PRE = {:?}", result),
         Err(error) => println!("- error: {:?}", error),
     };
 
-    match ctap_hid_fido2::enable_info_option(&HidParam::get_default_params(),InfoOption::CredMgmt) {
+    match ctap_hid_fido2::enable_info_option(&HidParam::get_default_params(), InfoOption::CredMgmt)
+    {
         Ok(result) => println!("CredMgmt = {:?}", result),
         Err(error) => println!("- error: {:?}", error),
     };
 
-    match ctap_hid_fido2::enable_info_option(&HidParam::get_default_params(),InfoOption::CredentialMgmtPreview) {
+    match ctap_hid_fido2::enable_info_option(
+        &HidParam::get_default_params(),
+        InfoOption::CredentialMgmtPreview,
+    ) {
         Ok(result) => println!("CredentialMgmtPreview = {:?}", result),
         Err(error) => println!("- error: {:?}", error),
     };
 
-    match ctap_hid_fido2::enable_info_option(&HidParam::get_default_params(),InfoOption::UserVerificationMgmtPreview) {
+    match ctap_hid_fido2::enable_info_option(
+        &HidParam::get_default_params(),
+        InfoOption::UserVerificationMgmtPreview,
+    ) {
         Ok(result) => println!("CredentialMgmtPreview = {:?}", result),
         Err(error) => println!("- error: {:?}", error),
     };
 
-    match ctap_hid_fido2::enable_info_option(&HidParam::get_default_params(),InfoOption::BioEnroll) {
+    match ctap_hid_fido2::enable_info_option(&HidParam::get_default_params(), InfoOption::BioEnroll)
+    {
         Ok(result) => println!("BioEnroll = {:?}", result),
         Err(error) => println!("- error: {:?}", error),
     };
 
-    if matches.is_present("info"){
+    if matches.is_present("info") {
         println!("get_info()");
         match ctap_hid_fido2::get_info(&HidParam::get_default_params()) {
             Ok(info) => println!("{}", info),
@@ -115,45 +127,35 @@ fn main() {
     if let Some(ref matches) = matches.subcommand_matches("bio_enrollment") {
         println!("used authenticatorBioEnrollment");
         println!("");
-        
-        if matches.is_present("info"){
+
+        if matches.is_present("info") {
             println!("Get fingerprint sensor info");
-            match ctap_hid_fido2::bio_enrollment_get_fingerprint_sensor_info(&HidParam::get_default_params())
-            {
-                Ok(result) => {
-                    println!("- {:?}", result);
-                }
-                Err(error) => {
-                    println!(
-                        "- bio_enrollment_get_fingerprint_sensor_info error: {:?}",
-                        error
-                    );
-                }
-            };
-            println!("");
+            match ctap_hid_fido2::bio_enrollment_get_fingerprint_sensor_info(
+                &HidParam::get_default_params(),
+            ) {
+                Ok(result) => println!("- {:?}", result),
+                Err(e) => println!("- error: {:?}", e),
+            }
             println!("");
         }
 
-        if matches.is_present("enumerate"){
+        if matches.is_present("enumerate") {
             println!("Enumerate enrollments");
             match ctap_hid_fido2::bio_enrollment_enumerate_enrollments(
                 &HidParam::get_default_params(),
                 Some(pin),
             ) {
-                Ok(template_infos) => {
-                    for i in template_infos.iter() {
-                        println!("- {}", i);
+                Ok(infos) => {
+                    for i in infos {
+                        println!("- {}", i)
                     }
                 }
-                Err(error) => {
-                    println!("- bio_enrollment_enumerate_enrollments error: {:?}", error);
-                }
-            };
-            println!("");
+                Err(e) => println!("- error: {:?}", e),
+            }
             println!("");
         }
 
-        if matches.is_present("rename"){
+        if matches.is_present("rename") {
             let mut values = matches.values_of("rename").unwrap();
             let template_id = values.next().unwrap();
             let name = values.next();
@@ -167,43 +169,33 @@ fn main() {
                 Some(pin),
                 TemplateInfo::new(util::to_str_hex(template_id), name),
             ) {
-                Ok(()) => {
-                    println!("- Success");
-                }
-                Err(error) => {
-                    println!("- bio_enrollment_enumerate_enrollments error: {:?}", error);
-                }
-            };
-            println!("");
+                Ok(_) => println!("- Success"),
+                Err(e) => println!("- error: {:?}", e),
+            }
             println!("");
         }
 
-        if matches.is_present("delete"){
+        if matches.is_present("delete") {
             let template_id = matches.value_of("delete").unwrap();
             println!("Delete enrollment");
             println!("- value for templateId: {:?}", template_id);
             println!("");
-        
+
             match ctap_hid_fido2::bio_enrollment_remove(
                 &HidParam::get_default_params(),
                 Some(pin),
                 util::to_str_hex(template_id),
             ) {
-                Ok(()) => {
-                    println!("- Success");
-                }
-                Err(error) => {
-                    println!("- bio_enrollment_remove error: {:?}", error);
-                }
-            };
-            println!("");
+                Ok(_) => println!("- Success"),
+                Err(e) => println!("- error: {:?}", e),
+            }
             println!("");
         }
 
-        if matches.is_present("enroll"){            
-            match bio_enrollment(pin){
-                Ok(()) => {},
-                Err(_) => {},
+        if matches.is_present("enroll") {
+            match bio_enrollment(pin) {
+                Ok(_) => {}
+                Err(e) => println!("- error: {:?}", e),
             }
         }
     }
@@ -223,50 +215,28 @@ fn main() {
     */
 }
 
-fn bio_enrollment(pin: &str)->Result<(),String>{
+fn bio_enrollment(pin: &str) -> Result<(), String> {
     println!("bio_enrollment_begin");
-    let enroll_status = match ctap_hid_fido2::bio_enrollment_begin(
+    let result = ctap_hid_fido2::bio_enrollment_begin(
         &HidParam::get_default_params(),
         Some(pin),
         Some(10000),
-    ) {
-        Ok(result) => {
-            println!("{}",result.1);
-            result.0
-        }
-        Err(error) => {
-            let msg = format!("- bio_enrollment_begin error: {:?}", error);
-            println!("{}",msg);
-            return Err(msg)
-        }
-    };
-    println!("");
+    )?;
+    println!("{}", result.1);
     println!("");
 
     for _counter in 0..10 {
-        if bio_enrollment_next(&enroll_status)? {
+        if bio_enrollment_next(&result.0)? {
             break;
         }
     }
     Ok(())
 }
 
-fn bio_enrollment_next(enroll_status: &EnrollStatus1)->Result<bool,String>{
+fn bio_enrollment_next(enroll_status: &EnrollStatus1) -> Result<bool, String> {
     println!("bio_enrollment_next");
-    match ctap_hid_fido2::bio_enrollment_next(
-        enroll_status,
-        Some(10000),
-    ) {
-        Ok(result) => {
-            println!("{}",result);
-            println!("");
-            println!("");
-            return Ok(result.is_finish)
-        }
-        Err(error) => {
-            let msg = format!("- bio_enrollment_next error: {:?}", error);
-            println!("{}",msg);
-            return Err(msg)
-        }
-    };
+    let result = ctap_hid_fido2::bio_enrollment_next(enroll_status, Some(10000))?;
+    println!("{}", result);
+    println!("");
+    Ok(result.is_finish)
 }
