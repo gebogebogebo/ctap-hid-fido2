93d7a7ff7828617c2fb665dd87643a55184d61cd
gebo <gebogebogeboge@gmail.com>
2021-05-03 17:13:33 +0900

    updaste

--- examples/nitro-update/src/main.rs
+++ examples/nitro-update/src/main.rs
@@ -1,7 +1,9 @@
-fn main() {
+use anyhow::Result;
+use serde_json::Value;
+use ihex::Record;
+use base64::decode;
 
-    println!("----- Nitrokey ENTERBOOT start -----");
-    // test
+fn bootloader() {
     let result = match ctap_hid_fido2::nitrokey::is_bootloader_mode(&ctap_hid_fido2::HidParam::get_default_params()){
         Ok(result) => result,
         Err(e) => {
@@ -21,6 +23,45 @@ fn main() {
             Err(err) => println!("enter boot Error = {}", err),
         };
     }
+}
 
+fn main() -> Result<()> {
+
+    println!("----- Nitrokey ENTERBOOT start -----");
+
+    if false {
+        bootloader();        
+    }
+
+    // read from json file
+    let firmware_json = std::fs::File::open("/Users/suzuki/tmp/nitro/fido2_firmware.json")?;
+    let v: Value = serde_json::from_reader(firmware_json)?;
+
+    // firmware-data => base64 => bin => string
+    let firmware_base64 = {
+        if let Value::String(v) = &v["firmware"] {
+            v.to_string()
+        }else{
+            "".to_string()
+        }
+    };
+    let firmware_dec = &decode(firmware_base64)?;
+    let firmware_str = String::from_utf8(firmware_dec.to_vec())?;
+    
+    let records = &[
+        Record::Data { offset: 0x0010, value: vec![0x48,0x65,0x6C,0x6C,0x6F] },
+        //Record::Data { offset: 0x0010, value: firmware_bin.to_vec() },
+        Record::EndOfFile
+    ];
+
+    let object = ihex::create_object_file_representation(records)?;
+    println!("{}", object);
+
+    //let signature = &v["versions"][">2.5.3"]["signature"];
+
+    
+    // read firmware file
     println!("----- Nitrokey ENTERBOOT end -----");
+
+    Ok(())
 }
