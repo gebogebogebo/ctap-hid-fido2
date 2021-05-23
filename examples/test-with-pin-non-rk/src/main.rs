9c37b6597dfa52c63f86f46a83ff3a0fabb7553d
gebo <gebogebogeboge@gmail.com>
2021-05-24 07:56:42 +0900

    ver 2.1.1

--- examples/test-with-pin-non-rk/src/main.rs
+++ examples/test-with-pin-non-rk/src/main.rs
@@ -9,6 +9,8 @@ fn main() -> Result<()> {
     println!("----- test-with-pin-non-rk start -----");
 
     // parameter
+    let hmac_make=false;
+    let hmac_get=false;
     let rpid = "test.com";
     let pin = "1234";
     let challenge = verifier::create_challenge();
@@ -21,29 +23,31 @@ fn main() -> Result<()> {
             .appent("Register - make_credential()")
             .append("- rpid", &rpid)
             .appenh("- challenge", &challenge)
+            .append("- hmac-secret", &hmac_make)
             .build()
     );
 
-    /*
-    let att = ctap_hid_fido2::make_credential(
-        &HidParam::get_default_params(),
-        rpid,
-        &challenge,
-        Some(pin),
-    )?;
-     */
+    let att = if hmac_make{
+        // with extensions
+        let ext = Mext::HmacSecret(Some(true));
+        ctap_hid_fido2::make_credential_with_extensions(
+            &HidParam::get_default_params(),
+            rpid,
+            &challenge,
+            Some(pin),
+            Some(&vec![ext]),
+        )?
+    }else{
+        ctap_hid_fido2::make_credential(
+            &HidParam::get_default_params(),
+            rpid,
+            &challenge,
+            Some(pin),
+        )?
+    };
 
-    // with extensions
-    let ext = Mext::HmacSecret(Some(true));
-    let att = ctap_hid_fido2::make_credential_with_extensions(
-        &HidParam::get_default_params(),
-        rpid,
-        &challenge,
-        Some(pin),
-        Some(&vec![ext]),
-    )?;
 
-    println!("- Register Success!!");
+    println!("!! Register Success !!");
     println!("Attestation");
     println!("{}", att);
 
@@ -60,46 +64,41 @@ fn main() -> Result<()> {
     );
 
     // Authenticate
-    println!("Authenticate - get_assertion_with_pin()");
     let challenge = verifier::create_challenge();
-    println!("{}", StrBuf::bufh("- challenge", &challenge));
+    let mut strbuf = StrBuf::new(30);
+    println!(
+        "{}",
+        strbuf
+            .appent("Authenticate - get_assertion_with_pin()")
+            .appenh("- challenge", &challenge)
+            .append("- hmac-secret", &hmac_get)
+            .build()
+    );
 
-    /*
-    let ass = ctap_hid_fido2::get_assertion(
-        &HidParam::get_default_params(),
-        rpid,
-        &challenge,
-        &verify_result.credential_id,
-        Some(pin),
-    )?;
-    */
-    let ext = Gext::create_hmac_secret_from_string("this is test");
-    let ass = ctap_hid_fido2::get_assertion_with_extensios(
-        &HidParam::get_default_params(),
-        rpid,
-        &challenge,
-        &verify_result.credential_id,
-        Some(pin),
-        Some(&vec![ext]),
-    )?;
 
-    println!("- Authenticate Success!!");
+    let ass = if hmac_get{
+        let ext = Gext::create_hmac_secret_from_string("this is test");
+        ctap_hid_fido2::get_assertion_with_extensios(
+            &HidParam::get_default_params(),
+            rpid,
+            &challenge,
+            &verify_result.credential_id,
+            Some(pin),
+            Some(&vec![ext]),
+        )?    
+    }else{
+        ctap_hid_fido2::get_assertion(
+            &HidParam::get_default_params(),
+            rpid,
+            &challenge,
+            &verify_result.credential_id,
+            Some(pin),
+        )?
+    };
+
+    println!("!! Authenticate Success !!");
     println!("Assertion");
     println!("{}", ass);
-
-    println!("Assertion - Extension");
-    for e in &ass.extensions {
-        match e {
-            Gext::HmacSecret(d) => {
-                if let Some(output1_enc) = d {
-                    println!(
-                        "{}",
-                        StrBuf::bufh("- AES256-CBC(SharedSecret,IV=0,output1)", output1_enc)
-                    );
-                }
-            }
-        }
-    }
 
     println!("Verify");
     let is_success = verifier::verify_assertion(
