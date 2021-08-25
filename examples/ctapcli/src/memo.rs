e154598d1ec0f56c8a33dff2ddfba0065b193e12
gebo <gebogebogeboge@gmail.com>
2021-08-26 07:42:40 +0900

    update

--- examples/ctapcli/src/memo.rs
+++ examples/ctapcli/src/memo.rs
@@ -16,62 +16,58 @@ use ctap_hid_fido2::verifier;
 
 pub fn memo(matches: &clap::ArgMatches) -> Result<()> {
 
+    // check
     if is_supported()? == false {
         return Err(anyhow!(
             "Sorry , This authenticator is not supported for this functions."
         ));
     }
 
+    // Title
+    if matches.is_present("add") {
+        println!("Add a memo.");
+    } else if matches.is_present("del") {
+        println!("Delete a memo.");
+    } else if matches.is_present("list") {
+        println!("List All memos.");
+    } else if matches.is_present("get") {
+        println!("Get a memo.");
+    } else {
+        println!("Get a memo.");
+    }
+
+    // PIN
     let pin = common::get_pin();
     let rpid = "ctapcli";
 
+    // main
     if matches.is_present("add") {
-        println!("Add a memo");
-        add(&pin,rpid)?;
+        let tag = common::get_input_with_message("tag:");
+        add(&tag, &pin, rpid)?;
+
     } else if matches.is_present("del") {
         let mut values = matches.values_of("del").unwrap();
         let tag = values.next().unwrap();
-        println!("Delete a memo => {}.", tag);
+        del(tag,&pin,rpid)?;
 
-        if let Some(cred) = search_cred(&pin, rpid, tag.as_bytes())? {
-            ctap_hid_fido2::credential_management_delete_credential(
-                &HidParam::get_default_params(),
-                Some(&pin),
-                Some(cred.public_key_credential_descriptor),
-            )?;
-
-            println!("Delete Success!.");
-        } else {
-            println!("tag not found...");
-        }
+    } else if matches.is_present("list") {
+        list(&pin,rpid)?;
     } else if matches.is_present("get") {
         let mut values = matches.values_of("get").unwrap();
         let tag = values.next().unwrap();
         get(tag, &pin, rpid)?;
-    } else if matches.is_present("list") {
-        println!("List All Memos.");
-        list(&pin,rpid)?;
     } else {
         list(&pin,rpid)?;
-
-        println!("tag:");
-        let tag = common::get_input();
-
+        let tag = common::get_input_with_message("tag:");
         get(&tag, &pin, rpid)?;
     }
 
     Ok(())
 }
 
-fn add(pin: &str, rpid: &str) -> Result<()> {
-    println!("");
-    println!("tag:");
-    let tag = common::get_input();
-    println!("");
-
+fn add(tag: &str, pin: &str, rpid: &str) -> Result<()> {
     if let None = search_cred(&pin, rpid, tag.as_bytes())? {
-        println!("memo:");
-        let memo = common::get_input();
+        let memo = common::get_input_with_message("memo:");
 
         let challenge = verifier::create_challenge();
         let rkparam =
@@ -89,6 +85,45 @@ fn add(pin: &str, rpid: &str) -> Result<()> {
     } else {
         println!("This tag already exists :(");
     }
+
+    Ok(())
+}
+
+fn del(tag: &str, pin: &str, rpid: &str) -> Result<()> {
+    if let Some(cred) = search_cred(&pin, rpid, tag.as_bytes())? {
+        ctap_hid_fido2::credential_management_delete_credential(
+            &HidParam::get_default_params(),
+            Some(&pin),
+            Some(cred.public_key_credential_descriptor),
+        )?;
+
+        println!("Delete Success!.");
+    } else {
+        println!("tag not found...");
+    }
+    Ok(())
+}
+
+fn list(pin: &str, rpid: &str) -> Result<()> {
+    let rps = get_rps(Some(pin))?;
+    let mut rps = rps
+        .iter()
+        .filter(|it| it.public_key_credential_rp_entity.id == rpid);
+
+    if let Some(r) = rps.next() {
+        let creds = get_creds(Some(&pin), r)?;
+
+        for id in creds
+            .iter()
+            .map(|it| it.public_key_credential_user_entity.id.to_vec())
+        {
+            let tag = String::from_utf8(id)?;
+            println!("- {}", tag);
+        }
+
+        println!("({}/10)", creds.len());
+    }
+    println!();
 
     Ok(())
 }
@@ -143,14 +178,8 @@ fn search_cred(pin: &str, rpid: &str, user_entity_id: &[u8]) -> Result<Option<Cr
 }
 
 fn get(tag: &str, pin: &str, rpid: &str) -> Result<()> {
-    println!("Get a memo => {}.", tag);
-
     if let Some(cred) = search_cred(pin, rpid, tag.as_bytes())? {
-        //let tag = String::from_utf8(cred.public_key_credential_user_entity.id)?;
-        //println!("- tag = {}", tag);
-
         let data = cred.public_key_credential_user_entity.name;
-        //println!("- {}", data);
 
         let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
         ctx.set_contents(data.to_owned()).unwrap();
@@ -159,31 +188,6 @@ fn get(tag: &str, pin: &str, rpid: &str) -> Result<()> {
     } else {
         println!("tag not found...");
     }
-    Ok(())
-}
-
-fn list(pin: &str, rpid: &str) -> Result<()> {
-    let rps = get_rps(Some(pin))?;
-    let mut rps = rps
-        .iter()
-        .filter(|it| it.public_key_credential_rp_entity.id == rpid);
-
-    println!();
-    if let Some(r) = rps.next() {
-        let creds = get_creds(Some(&pin), r)?;
-
-        for id in creds
-            .iter()
-            .map(|it| it.public_key_credential_user_entity.id.to_vec())
-        {
-            let tag = String::from_utf8(id)?;
-            println!("- {}", tag);
-        }
-
-        println!("({}/10)", creds.len());
-    }
-    println!();
-
     Ok(())
 }
 
