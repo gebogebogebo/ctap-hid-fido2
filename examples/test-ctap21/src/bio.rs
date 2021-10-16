5ce17abd9b96a30276c8f2a91df22899e659d86f
gebo <gebogebogeboge@gmail.com>
2021-10-17 08:11:12 +0900

    Update bio.rs

--- examples/test-ctap21/src/bio.rs
+++ examples/test-ctap21/src/bio.rs
@@ -2,7 +2,6 @@ use anyhow::Result;
 use ctap_hid_fido2;
 
 use ctap_hid_fido2::bio_enrollment_params::EnrollStatus1;
-use ctap_hid_fido2::bio_enrollment_params::TemplateInfo;
 #[allow(unused_imports)]
 use ctap_hid_fido2::util;
 use ctap_hid_fido2::Key;
@@ -25,56 +24,6 @@ pub fn bio_main(matches: &clap::ArgMatches, pin: Option<&str>) -> Result<()> {
     }
 
     if matches.is_present("enumerate") {
-        println!("Enumerate enrollments");
-        match ctap_hid_fido2::bio_enrollment_enumerate_enrollments(
-            &Key::auto(),
-            pin,
-        ) {
-            Ok(infos) => {
-                for i in infos {
-                    println!("- {}", i)
-                }
-            }
-            Err(e) => println!("- error: {:?}", e),
-        }
-        println!("");
-    }
-
-    if matches.is_present("rename") {
-        let mut values = matches.values_of("rename").unwrap();
-        let template_id = values.next().unwrap();
-        let name = values.next();
-        println!("Rename/Set FriendlyName");
-        println!("- value for templateId: {:?}", template_id);
-        println!("- value for templateFriendlyName: {:?}", name);
-        println!("");
-
-        match ctap_hid_fido2::bio_enrollment_set_friendly_name(
-            &Key::auto(),
-            pin,
-            TemplateInfo::new(util::to_str_hex(template_id), name),
-        ) {
-            Ok(_) => println!("- Success"),
-            Err(e) => println!("- error: {:?}", e),
-        }
-        println!("");
-    }
-
-    if matches.is_present("delete") {
-        let template_id = matches.value_of("delete").unwrap();
-        println!("Delete enrollment");
-        println!("- value for templateId: {:?}", template_id);
-        println!("");
-
-        match ctap_hid_fido2::bio_enrollment_remove(
-            &Key::auto(),
-            pin,
-            util::to_str_hex(template_id),
-        ) {
-            Ok(_) => println!("- Success"),
-            Err(e) => println!("- error: {:?}", e),
-        }
-        println!("");
     }
 
     if matches.is_present("enroll") {
@@ -89,19 +38,6 @@ pub fn bio_main(matches: &clap::ArgMatches, pin: Option<&str>) -> Result<()> {
 
 fn bio_enrollment(pin: &str) -> Result<()> {
     println!("bio_enrollment_begin");
-    let result = ctap_hid_fido2::bio_enrollment_begin(
-        &Key::auto(),
-        Some(pin),
-        Some(10000),
-    )?;
-    println!("{}", result.1);
-    println!("");
-
-    for _counter in 0..10 {
-        if bio_enrollment_next(&result.0)? {
-            break;
-        }
-    }
     Ok(())
 }
 
