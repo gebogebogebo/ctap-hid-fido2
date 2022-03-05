dbc7dccf29c937694028af599a669a8d3007ad6d
gebogebo <gebogebogeboge@gmail.com>
2022-03-05 10:55:20 +0900

    update

--- README_Register_and_Authenticate.md
+++ README_Register_and_Authenticate.md
@@ -6,195 +6,19 @@
 
 ### non-discoverable credentials/non-resident-key
 
-```rust
-use anyhow::Result;
-use ctap_hid_fido2;
-use ctap_hid_fido2::util;
-use ctap_hid_fido2::verifier;
-use ctap_hid_fido2::Cfg;
-
-fn main() -> Result<()> {
-    println!("----- test-with-pin-non-rk start -----");
-
-    // parameter
-    let rpid = "test.com";
-    let pin = Some("1234");
-    // let pin = None; // Yubikey bio for fingerprint authentication
-    let challenge = verifier::create_challenge();
-
-    // Register
-    println!("Register - make_credential()");
-    println!("- rpid          = {:?}", rpid);
-    println!(
-        "- challenge({:02}) = {:?}",
-        challenge.len(),
-        util::to_hex_str(&challenge)
-    );
-
-    let att = ctap_hid_fido2::make_credential(
-        &Cfg::init(),
-        rpid,
-        &challenge,
-        pin,
-    )?;
-
-    println!("- Register Success!!");
-    println!("Attestation");
-    println!("{}", att);
-
-    println!("Verify");
-    let verify_result = verifier::verify_attestation(rpid, &challenge, &att);
-    println!(
-        "- is_success                   = {:?}",
-        verify_result.is_success
-    );
-    println!(
-        "- credential_id({:02})            = {:?}",
-        verify_result.credential_id.len(),
-        util::to_hex_str(&verify_result.credential_id)
-    );
-    println!(
-        "- credential_publickey_der({:02}) = {:?}",
-        verify_result.credential_publickey_der.len(),
-        util::to_hex_str(&verify_result.credential_publickey_der)
-    );
-    println!("");
-
-    // Authenticate
-    println!("Authenticate - get_assertion_with_pin()");
-    let challenge = verifier::create_challenge();
-    println!(
-        "- challenge({:02}) = {:?}",
-        challenge.len(),
-        util::to_hex_str(&challenge)
-    );
-
-    let ass = ctap_hid_fido2::get_assertion(
-        &Cfg::init(),
-        rpid,
-        &challenge,
-        &verify_result.credential_id,
-        pin,
-    )?;
-    println!("- Authenticate Success!!");
-    println!("Assertion");
-    println!("{}", ass);
-
-    println!("Verify");
-    let is_success = verifier::verify_assertion(
-        rpid,
-        &verify_result.credential_publickey_der,
-        &challenge,
-        &ass,
-    );
-    println!("- is_success = {:?}", is_success);
-
-    println!("----- test-with-pin-non-rk end -----");
-    Ok(())
-}
-```
+→ link
 
 - If you want to use Yubikey bio for fingerprint authentication, specify None for pin.
-
-**console**
-
-```sh
------ test-with-pin-non-rk start -----
-Register - make_credential()
-- rpid          = "test.com"
-- challenge(32) = "054E416942D60F9F584B58A1E53B3ED85E9ECBB486D72CE690569E884B038267"
-- touch fido key
-- Register Success!!
-Attestation
-- rpid_hash(32)                           = 99AB715D84A3BC5E0E92AA50E67A5813637FD1744BD301AB08F87191DDB816E0
-- flags_user_present_result               = true
-- flags_user_verified_result              = true
-- flags_attested_credential_data_included = true
-- flags_extension_data_included           = false
-- sign_count                              = 1
-- aaguid(16)                              = EE882879721C491397753DFCCE97072A
-- credential_descriptor                   = (id : 4AE2... , type : )
-- credential_publickey                    = (der : 04A0... , pem : -----BEGIN PUBLIC KEY-----...)
-- attstmt_alg                             = -7
-- attstmt_sig(71)                         = 3045...
-- attstmt_x5c_num                         = 1
-Verify
-- is_success                   = true
-- credential_id(64)            = "4AEA..."
-- credential_publickey_der(65) = "04A0..."
-
-Authenticate - get_assertion_with_pin()
-- challenge(32) = "0B1A3BF49C6D335592EE789C9C662365E06F4D9A63E6C4EA5B62B221E072A33E"
-- touch fido key
-- Authenticate Success!!
-Assertion
-- rpid_hash(32)                           = 99AB715D84A3BC5E0E92AA50E67A5813637FD1744BD301AB08F87191DDB816E0
-- flags_user_present_result               = true
-- flags_user_verified_result              = true
-- flags_attested_credential_data_included = false
-- flags_extension_data_included           = false
-- sign_count                              = 4
-- number_of_credentials                   = 0
-- signature(71)                           = 3045...
-- user                                    = (id :  , name :  , display_name : )
-- credential_id(64)                       = 4AEA...
-Verify
-- is_success = true
------ test-with-pin-non-rk end -----
-```
 
 
 
 #### Using Key Type
 
-TODO
+→ link
 
+- Verify Assertion in Ed25519 is always false because it is not yet implemented
 
-
-#### Using HMAC Secret Extension
-
-**Register**
-
-```rust
-fn main() -> Result<()> {
-  let rpid = "test.com";
-  let pin = Some("1234");
-  let challenge = verifier::create_challenge();
-
-  let ext = Mext::HmacSecret(Some(true));
-  let att = ctap_hid_fido2::make_credential_with_extensions(
-      &Cfg::init(),
-      rpid,
-      &challenge,
-      pin,
-      Some(&vec![ext]),
-  )?;
-}
-```
-
-**Authenticate**
-
-```rust
-fn main() -> Result<()> {
-
-  let rpid = "test.com";
-  let pin = Some("1234");
-  let challenge = verifier::create_challenge();
-  let credential_id = ???;
   
-  let ext = Gext::create_hmac_secret_from_string("this is salt");
-  let ass = ctap_hid_fido2::get_assertion_with_extensios(
-      &Cfg::init(),
-      rpid,
-      &challenge,
-      &credential_id,
-      pin,
-      Some(&vec![ext]),
-  )?;
-}
-```
-
-
 
 ### discoverable credentials/resident-key
 
@@ -289,68 +113,25 @@ fn main() -> Result<()> {
 
 ### non-discoverable credentials/non-resident-key
 
-```rust
-// ex.
-// let cfg = Cfg::init();
-// let rpid = "test.com";
-// let pin = "1234";
-fn non_discoverable_credentials(cfg: &Cfg, rpid: &str, pin: &str) -> Result<()> {
-  println!("----- non_discoverable_credentials -----");
-
-  println!("- Register");
-  let challenge = verifier::create_challenge();
-
-  let make_credential_args = MakeCredentialArgsBuilder::new(&rpid, &challenge)
-  .pin(pin)
-  .build();
-
-  let attestation = ctap_hid_fido2::make_credential_with_args(&cfg, &make_credential_args)?;
-  println!("-- Register Success");
-
-  println!("-- Verify Attestation");
-  let verify_result = verifier::verify_attestation(rpid, &challenge, &attestation);
-  if verify_result.is_success {
-    println!("-- Verify Attestation Success");
-  } else {
-    println!("-- Verify Attestation Failed");
-  }
-
-  println!("- Authenticate");
-  let challenge = verifier::create_challenge();
-
-  let get_assertion_args = ctap_hid_fido2::GetAssertionArgsBuilder::new(&rpid, &challenge)
-  .pin(pin)
-  .credential_id(&verify_result.credential_id)
-  .build();
-
-  let assertions = ctap_hid_fido2::get_assertion_with_args(cfg,&get_assertion_args)?;
-  println!("-- Authenticate Success");
-
-  println!("-- Verify Assertion");
-  let is_success = verifier::verify_assertion(
-    rpid,
-    &verify_result.credential_publickey_der,
-    &challenge,
-    &assertions[0],
-  );
-  if is_success {
-    println!("-- Verify Assertion Success");
-  } else {
-    println!("-- Verify Assertion Failed");
-  }
-
-  Ok(())
-}
-```
+→ link
 
 
 
-#### Using Key Type
+#### with Key Type
+
+→ link
+
+Verify Assertion in Ed25519 is always false because it is not yet implemented
 
 
 
-#### Using HMAC Secret Extension
+#### with HMAC Secret Extension
+
+→ link
 
 
 
 ### discoverable credentials/resident-key
+
+→ link
+
--- examples/test-with-pin-non-rk/src/main.rs
+++ examples/test-with-pin-non-rk/src/main.rs
@@ -21,126 +21,27 @@ fn main() -> Result<()> {
 
     builder_pattern_sample(&cfg, rpid, pin)?;
 
-    /*
-    // parameter
-    let hmac_make=true;
-    let hmac_get=true;
-    let rpid = "test.com";
-    let pin = Some("1234");
-    //let pin = None;
-    let challenge = verifier::create_challenge();
-
-
-    // Register
-    let mut strbuf = StrBuf::new(30);
-    println!(
-        "{}",
-        strbuf
-            .appent("Register - make_credential()")
-            .append("- rpid", &rpid)
-            .appenh("- challenge", &challenge)
-            .append("- hmac-secret", &hmac_make)
-            .build()
-    );
-
-    let att = if hmac_make{
-        // with extensions
-        let ext = Mext::HmacSecret(Some(true));
-        ctap_hid_fido2::make_credential_with_extensions(
-            &cfg,
-            rpid,
-            &challenge,
-            pin,
-            Some(&vec![ext]),
-        )?
-    }else{
-        ctap_hid_fido2::make_credential(
-            &cfg,
-            rpid,
-            &challenge,
-            pin
-        )?
-    };
-
-
-    println!("!! Register Success !!");
-    println!("Attestation");
-    println!("{}", att);
-
-    println!("Verify");
-    let verify_result = verifier::verify_attestation(rpid, &challenge, &att);
-
-    let mut strbuf = StrBuf::new(30);
-    println!(
-        "{}",
-        strbuf
-            .append("- is_success", &verify_result.is_success)
-            .appenh("- credential_id", &verify_result.credential_id)
-            .build()
-    );
-
-    // Authenticate
-    let challenge = verifier::create_challenge();
-    let mut strbuf = StrBuf::new(30);
-    println!(
-        "{}",
-        strbuf
-            .appent("Authenticate - get_assertion_with_pin()")
-            .appenh("- challenge", &challenge)
-            .append("- hmac-secret", &hmac_get)
-            .build()
-    );
-
-
-    let ass = if hmac_get{
-        let ext = Gext::create_hmac_secret_from_string("this is test");
-        ctap_hid_fido2::get_assertion_with_extensios(
-            &cfg,
-            rpid,
-            &challenge,
-            &verify_result.credential_id,
-            pin,
-            Some(&vec![ext]),
-        )?
-    }else{
-        ctap_hid_fido2::get_assertion(
-            &cfg,
-            rpid,
-            &challenge,
-            &verify_result.credential_id,
-            pin,
-        )?
-    };
-
-    println!("!! Authenticate Success !!");
-    println!("Assertion");
-    println!("{}", ass);
-
-    println!("Verify");
-    let is_success = verifier::verify_assertion(
-        rpid,
-        &verify_result.credential_publickey_der,
-        &challenge,
-        &ass,
-    );
-    println!("- is_success = {:?}", is_success);
-    */
+    legacy_pattern_sample(&cfg, rpid, pin)?;
 
     println!("----- test-with-pin-non-rk end -----");
     Ok(())
 }
 
+//
+// Builder Pattern Sample
+//
 fn builder_pattern_sample(cfg: &Cfg, rpid: &str, pin: &str) -> Result<()> {
-    with_hmac(cfg, rpid, pin)
-    .unwrap_or_else(|err| eprintln!("Error => {}", err));
-
     non_discoverable_credentials(cfg, rpid, pin)
     .unwrap_or_else(|err| eprintln!("Error => {}", err));
 
     with_key_type(cfg, rpid, pin, CredentialSupportedKeyType::Ecdsa256)
     .unwrap_or_else(|err| eprintln!("Error => {}", err));
 
+    // Verify Assertion in Ed25519 is always false because it is not yet implemented
     with_key_type(cfg, rpid, pin, CredentialSupportedKeyType::Ed25519)
+    .unwrap_or_else(|err| eprintln!("Error => {}", err));
+
+    with_hmac(cfg, rpid, pin)
     .unwrap_or_else(|err| eprintln!("Error => {}", err));
 
     Ok(())
@@ -310,3 +211,128 @@ fn with_hmac(cfg: &Cfg,rpid: &str, pin: &str) -> Result<()> {
     Ok(())
 }
 
+//
+// Legacy Pattern Sample
+//
+fn legacy_pattern_sample(cfg: &Cfg, rpid: &str, pin: &str) -> Result<()> {
+    legacy_non_discoverable_credentials(cfg, rpid, pin)
+    .unwrap_or_else(|err| eprintln!("Error => {}", err));
+
+    legacy_with_key_type(cfg, rpid, pin, CredentialSupportedKeyType::Ecdsa256)
+    .unwrap_or_else(|err| eprintln!("Error => {}", err));
+
+    // Verify Assertion in Ed25519 is always false because it is not yet implemented
+    legacy_with_key_type(cfg, rpid, pin, CredentialSupportedKeyType::Ed25519)
+    .unwrap_or_else(|err| eprintln!("Error => {}", err));
+
+    Ok(())
+}
+
+fn legacy_non_discoverable_credentials(cfg: &Cfg, rpid: &str, pin: &str) -> Result<()> {
+    println!("----- legacy_non_discoverable_credentials -----");
+
+    println!("- Register");
+    let challenge = verifier::create_challenge();
+    let attestation = ctap_hid_fido2::make_credential(
+        cfg,
+        rpid,
+        &challenge,
+        Some(pin),
+    )?;
+
+    println!("-- Register Success");
+    //println!("Attestation");
+    //println!("{}", attestation);
+
+    println!("-- Verify Attestation");
+    let verify_result = verifier::verify_attestation(rpid, &challenge, &attestation);
+    if verify_result.is_success {
+        println!("-- Verify Attestation Success");
+    } else {
+        println!("-- ! Verify Attestation Failed");
+    }
+
+    println!("- Authenticate");
+    let challenge = verifier::create_challenge();
+    let assertion = ctap_hid_fido2::get_assertion(
+        cfg,
+        rpid,
+        &challenge,
+        &verify_result.credential_id,
+        Some(pin),
+    )?;
+    println!("-- Authenticate Success");
+    //println!("Assertion");
+    //println!("{}", assertions[0]);
+
+    println!("-- Verify Assertion");
+    let is_success = verifier::verify_assertion(
+        rpid,
+        &verify_result.credential_publickey_der,
+        &challenge,
+        &assertion,
+    );
+    if is_success {
+        println!("-- Verify Assertion Success");
+    } else {
+        println!("-- ! Verify Assertion Failed");
+    }
+
+    println!();
+    Ok(())
+}
+
+fn legacy_with_key_type(cfg: &Cfg, rpid: &str, pin: &str, key_type: CredentialSupportedKeyType) -> Result<()> {
+    println!("----- legacy_with_key_type ({:?}) -----", key_type);
+
+    println!("- Register");
+    let challenge = verifier::create_challenge();
+    let attestation = ctap_hid_fido2::make_credential_with_key_type(
+        cfg,
+        rpid,
+        &challenge,
+        Some(pin),
+        Some(key_type),
+    )?;
+
+    println!("-- Register Success");
+    //println!("Attestation");
+    //println!("{}", attestation);
+
+    println!("-- Verify Attestation");
+    let verify_result = verifier::verify_attestation(rpid, &challenge, &attestation);
+    if verify_result.is_success {
+        println!("-- Verify Attestation Success");
+    } else {
+        println!("-- ! Verify Attestation Failed");
+    }
+
+    println!("- Authenticate");
+    let challenge = verifier::create_challenge();
+    let assertion = ctap_hid_fido2::get_assertion(
+        cfg,
+        rpid,
+        &challenge,
+        &verify_result.credential_id,
+        Some(pin),
+    )?;
+    println!("-- Authenticate Success");
+    //println!("Assertion");
+    //println!("{}", assertions[0]);
+
+    println!("-- Verify Assertion");
+    let is_success = verifier::verify_assertion(
+        rpid,
+        &verify_result.credential_publickey_der,
+        &challenge,
+        &assertion,
+    );
+    if is_success {
+        println!("-- Verify Assertion Success");
+    } else {
+        println!("-- ! Verify Assertion Failed");
+    }
+
+    println!();
+    Ok(())
+}
