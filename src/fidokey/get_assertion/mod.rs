9f6d6e0448da51199387f9588ef3bf7abe50480b
gebogebo <gebogebogeboge@gmail.com>
2022-04-09 09:23:22 +0900

    Update mod.rs

--- src/fidokey/get_assertion/mod.rs
+++ src/fidokey/get_assertion/mod.rs
@@ -70,11 +70,10 @@ impl FidoKeyHid {
 
     /// Create a new assertion manually specifying the args using GetAssertionArgs
     pub fn get_assertion_with_args(&self, args: &GetAssertionArgs) -> Result<Vec<Assertion>> {
-        let dummy_credentials; // TODO ???
+        let dummy_credentials;
         let credential_ids = if args.credential_ids.len() > 0 {
             &args.credential_ids
         } else {
-            // TODO ???
             dummy_credentials = vec![];
             &dummy_credentials
         };
