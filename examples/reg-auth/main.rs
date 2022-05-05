0232e5ca46286320816b2cb2750c6147059bd570
gebogebo <gebogebogeboge@gmail.com>
2022-05-06 07:59:03 +0900

    readme

--- examples/reg-auth/main.rs
+++ examples/reg-auth/main.rs
@@ -8,40 +8,48 @@ fn main() {
     let pin = get_input_with_message("input PIN:");
 
     println!("Register");
-    // create `challenge` (Correct implementation is done on the server side)
+    // create `challenge`
     let challenge = verifier::create_challenge();
+
     // create `MakeCredentialArgs`
     let make_credential_args = MakeCredentialArgsBuilder::new(rpid, &challenge)
         .pin(&pin)
         .build();
+
     // create `FidoKeyHid`
     let device = FidoKeyHidFactory::create(&Cfg::init()).unwrap();
+
     // get `Attestation` Object
     let attestation = device
         .make_credential_with_args(&make_credential_args)
         .unwrap();
     println!("- Register Success");
-    // verify `Attestation` Object (Correct implementation is done on the server side)
+
+    // verify `Attestation` Object
     let verify_result = verifier::verify_attestation(rpid, &challenge, &attestation);
     if !verify_result.is_success {
         println!("- ! Verify Failed");
         return;
     }
-    // store Credential Id and Publickey (Correct implementation is done on the server side)
+
+    // store Credential Id and Publickey
     let userdata_credential_id = verify_result.credential_id;
     let userdata_credential_publickey_der = verify_result.credential_publickey_der;
 
     println!("Authenticate");
-    // create `challenge` (Correct implementation is done on the server side)
+    // create `challenge`
     let challenge = verifier::create_challenge();
+
     // create `GetAssertionArgs`
     let get_assertion_args = GetAssertionArgsBuilder::new(rpid, &challenge)
         .pin(&pin)
         .credential_id(&userdata_credential_id)
         .build();
-    // get `Assertion` Object (Correct implementation is done on the server side)
+
+    // get `Assertion` Object
     let assertions = device.get_assertion_with_args(&get_assertion_args).unwrap();
     println!("- Authenticate Success");
+
     // verify `Assertion` Object
     if !verifier::verify_assertion(
         rpid,
