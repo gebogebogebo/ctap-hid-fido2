0f8e9941e38df45df17e641708f417399d7964d2
gebo <35388172+gebogebogebo@users.noreply.github.com>
2025-03-29 15:59:16 +0900

    Update make_credential_command.rs

--- src/fidokey/make_credential/make_credential_command.rs
+++ src/fidokey/make_credential/make_credential_command.rs
@@ -23,10 +23,10 @@ pub struct Params {
 }
 
 impl Params {
-    pub fn new(rp_id: &str, challenge: Vec<u8>, user_id: Vec<u8>) -> Params {
-        Params {
+    pub fn new(rp_id: &str, challenge: Vec<u8>, user_id: Vec<u8>) -> Self {
+        Self {
             rp_id: rp_id.to_string(),
-            user_id: user_id.to_vec(),
+            user_id,
             client_data_hash: util::create_clientdata_hash(challenge),
             key_types: vec![CredentialSupportedKeyType::Ecdsa256],
             ..Default::default()
@@ -42,60 +42,48 @@ pub fn create_payload(params: Params, extensions: Option<&Vec<Extension>>) -> Re
     let rp = vec![
         ("id".to_value(), params.rp_id.to_value()),
         ("name".to_value(), params.rp_name.to_value()),
-    ].to_value();
+    ]
+    .to_value();
 
     // 0x03 : user
-    // user id
     let user_id = if params.user_id.is_empty() {
         vec![0x00]
     } else {
-        params.user_id.to_vec()
+        params.user_id
     };
-
-    // user name
     let user_name = if params.user_name.is_empty() {
         " ".to_string()
     } else {
-        params.user_name.to_string()
+        params.user_name
     };
-
-    // displayName
     let display_name = if params.user_display_name.is_empty() {
         " ".to_string()
     } else {
-        params.user_display_name.to_string()
+        params.user_display_name
     };
-
     let user = vec![
         ("id".to_value(), user_id.to_value()),
         ("name".to_value(), user_name.to_value()),
         ("displayName".to_value(), display_name.to_value()),
-    ].to_value();
+    ]
+    .to_value();
 
     // 0x04 : pubKeyCredParams
     let pub_key_cred_params = create_pub_key_cred_params(&params.key_types);
-
-    // TODO これのテストをやりたい
     // 0x05 : excludeList
     let exclude_list = create_exclude_list(&params.exclude_list);
-
     // 0x06 : extensions
-    let extensions = create_extensions(extensions);
-
+    let ext_val = create_extensions(extensions);
     // 0x07 : options
     let options = create_options(params.option_rk, params.option_up, params.option_uv);
-
-    // pinAuth(0x08)
-    let pin_auth = {
-        if !params.pin_auth.is_empty() {
-            Some(params.pin_auth.to_value())
-        } else {
-            None
-        }
+    // 0x08 : pinAuth (必要なら)
+    let pin_auth = if params.pin_auth.is_empty() {
+        None
+    } else {
+        Some(params.pin_auth.to_value())
     };
 
-
-    // create cbor object
+    // CBOR マップの各フィールドをキー（0x01～）と値のタプルとして格納
     let mut make_credential = vec![
         (0x01.to_value(), cdh),
         (0x02.to_value(), rp),
@@ -106,16 +94,13 @@ pub fn create_payload(params: Params, extensions: Option<&Vec<Extension>>) -> Re
     if !params.exclude_list.is_empty() {
         make_credential.push((0x05.to_value(), exclude_list));
     }
-
-    if let Some(x) = extensions {
-        make_credential.push((0x06.to_value(), x));
+    if let Some(ext) = ext_val {
+        make_credential.push((0x06.to_value(), ext));
     }
-
     make_credential.push((0x07.to_value(), options));
-    
-    if let Some(x) = pin_auth {
-        make_credential.push((0x08.to_value(), x));
-        // 0x09:pinProtocol
+    if let Some(pin) = pin_auth {
+        make_credential.push((0x08.to_value(), pin));
+        // 0x09: pinProtocol
         make_credential.push((0x09.to_value(), 1.to_value()));
     }
 
@@ -123,70 +108,62 @@ pub fn create_payload(params: Params, extensions: Option<&Vec<Extension>>) -> Re
 }
 
 fn create_pub_key_cred_params(key_types: &[CredentialSupportedKeyType]) -> Value {
-    let pub_key_cred_params_vec: Vec<_> = key_types
+    let params_vec: Vec<Value> = key_types
         .iter()
-        .map(|key_type| {
+        .map(|&key_type| {
             vec![
-                ("alg".to_value(), (*key_type as i64).to_value() ),
+                ("alg".to_value(), (key_type as i64).to_value()),
                 ("type".to_value(), "public-key".to_value()),
-            ].to_value()
+            ]
+            .to_value()
         })
         .collect();
+    params_vec.to_value()
+}
 
-    pub_key_cred_params_vec.to_value()
+fn create_exclude_list(credential_ids: &[Vec<u8>]) -> Value {
+    let list: Vec<Value> = credential_ids
+        .iter()
+        .map(|id| {
+            vec![
+                ("id".to_value(), id.to_value()),
+                ("type".to_value(), "public-key".to_value()),
+            ]
+            .to_value()
+        })
+        .collect();
+    list.to_value()
 }
 
 fn create_extensions(extensions: Option<&Vec<Extension>>) -> Option<Value> {
-    if let Some(extensions) = extensions {
-        let mut map = Vec::new();
-        for ext in extensions {
-            match *ext {
-                Extension::CredBlob((ref n, _)) => {
-                    let x = n.clone().unwrap();
-                    map.push((ext.to_string().to_value(), x.to_value()));
+    extensions.map(|exts| {
+        let map: Vec<(Value, Value)> = exts
+            .iter()
+            .map(|ext| match ext {
+                Extension::CredBlob((n, _)) => {
+                    (ext.to_string().to_value(), n.clone().unwrap().to_value())
                 }
                 Extension::CredProtect(n) => {
-                    map.push((
-                        ext.to_string().to_value(),
-                        (n.unwrap() as i64).to_value()
-                    ));
+                    (ext.to_string().to_value(), (n.unwrap() as i64).to_value())
                 }
                 Extension::HmacSecret(n)
                 | Extension::LargeBlobKey((n, _))
                 | Extension::MinPinLength((n, _)) => {
-                    map.push((ext.to_string().to_value(), n.unwrap().to_value()));
+                    (ext.to_string().to_value(), n.unwrap().to_value())
                 }
-            };
-        }
-        Some(map.to_value())
-    } else {
-        None
-    }
+            })
+            .collect();
+        map.to_value()
+    })
 }
-
 
 fn create_options(rk: bool, up: Option<bool>, uv: Option<bool>) -> Value {
-    let mut options_val = Vec::new();
-    options_val.push(("rk".to_value(), rk.to_value()));
+    let mut options = vec![("rk".to_value(), rk.to_value())];
     if let Some(v) = up {
-        options_val.push(("up".to_value(), v.to_value()));
+        options.push(("up".to_value(), v.to_value()));
     }
     if let Some(v) = uv {
-        options_val.push(("uv".to_value(), v.to_value()));
+        options.push(("uv".to_value(), v.to_value()));
     }
-    options_val.to_value()
-}
-
-fn create_exclude_list(credential_ids: &[Vec<u8>]) -> Value {
-    let exclude_list_vec: Vec<_> = credential_ids
-        .iter()
-        .map(|credential_id| {
-            vec![
-                ("id".to_value(), credential_id.to_value()),
-                ("type".to_value(), "public-key".to_value()),
-            ].to_value()
-        })
-        .collect();
-
-    exclude_list_vec.to_value()
+    options.to_value()
 }
