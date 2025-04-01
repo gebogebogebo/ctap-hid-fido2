839fc11c4c9e000742c20a1266d3600c3f4a9ea9
gebo <35388172+gebogebogebo@users.noreply.github.com>
2025-04-01 17:19:22 +0900

    get_assertion_command

--- src/fidokey/get_assertion/get_assertion_command.rs
+++ src/fidokey/get_assertion/get_assertion_command.rs
@@ -1,10 +1,11 @@
 use super::get_assertion_params::Extension;
 use crate::ctapdef;
+use crate::fidokey::common;
 use crate::hmac_ext::HmacExt;
-use crate::util::{self, vec_to_btree_map};
-use serde_cbor::to_vec;
-use serde_cbor::Value;
-use crate::util_ciborium;
+use crate::util;
+use crate::util_ciborium::ToValue;
+use anyhow::Result;
+use ciborium::value::Value;
 
 #[derive(Debug, Default)]
 pub struct Params {
@@ -31,120 +32,111 @@ pub fn create_payload(
     params: Params,
     extensions: Option<&Vec<Extension>>,
     hmac_ext: Option<HmacExt>,
-) -> Vec<u8> {
+) -> Result<Vec<u8>> {
     // 0x01 : rpid
-    let rpid = Value::Text(params.rp_id.to_string());
+    let rpid = params.rp_id.to_value();
 
     // 0x02 : clientDataHash
-    let cdh = Value::Bytes(params.client_data_hash);
+    let cdh = params.client_data_hash.to_value();
 
     // 0x03 : allowList
-    let allow_list = {
-        if !params.allowlist_credential_ids.is_empty() {
-            let allow_list = Value::Array(
-                params
-                    .allowlist_credential_ids
-                    .iter()
-                    .cloned()
-                    .map(|credential_id| {
-                        let allow_list_val = vec![
-                            (Value::Text("id".to_string()), Value::Bytes(credential_id)),
-                            (
-                                Value::Text("type".to_string()),
-                                Value::Text("public-key".to_string()),
-                            ),
-                        ];
-                        Value::Map(vec_to_btree_map(allow_list_val))
-                    })
-                    .collect(),
-            );
-            Some(allow_list)
-        } else {
-            None
-        }
-    };
+    let allow_list = create_allow_list(&params.allowlist_credential_ids);
 
     // 0x04 : extensions
-    let extensions = {
-        let mut ext_val = Vec::new(); // 複数の条件分岐で追加されるのでvec![]に変更しない
-
-        // HMAC Secret Extension
-        if let Some(hmac_ext) = hmac_ext {
-            let tmp = hmac_ext.shared_secret.public_key.to_value_cib().unwrap();
-            let val = util_ciborium::ciborium_to_serde(tmp).unwrap();
-            let param = vec![
-                // keyAgreement(0x01)
-                (Value::Integer(0x01), val),
-                // saltEnc(0x02)
-                (Value::Integer(0x02), Value::Bytes(hmac_ext.salt_enc)),
-                // saltAuth(0x03)
-                (Value::Integer(0x03), Value::Bytes(hmac_ext.salt_auth)),
-            ];
-
-            ext_val.push((
-                Value::Text(Extension::HmacSecret(None).to_string()),
-                Value::Map(vec_to_btree_map(param)),
-            ));
-        }
-
-        if let Some(extensions) = extensions {
-            for ext in extensions {
-                match *ext {
-                    Extension::HmacSecret(_) => (),
-                    Extension::LargeBlobKey((n, _)) | Extension::CredBlob((n, _)) => {
-                        ext_val.push((Value::Text(ext.to_string()), Value::Bool(n.unwrap())));
-                    }
-                };
-            }
-        }
-        if ext_val.is_empty() {
-            None
-        } else {
-            Some(Value::Map(vec_to_btree_map(ext_val)))
-        }
-    };
+    let ext_val = create_extensions(extensions, hmac_ext);
 
     // 0x05 : options
-    let mut options_val = vec![(Value::Text("up".to_string()), Value::Bool(params.option_up))];
-    if let Some(v) = params.option_uv {
-        options_val.push((Value::Text("uv".to_string()), Value::Bool(v)));
-    }
+    let options = create_options(params.option_up, params.option_uv);
 
-    let options = Value::Map(vec_to_btree_map(options_val));
-
-    // pinAuth(0x06)
-    let pin_auth = {
-        if !params.pin_auth.is_empty() {
-            Some(Value::Bytes(params.pin_auth))
-        } else {
-            None
-        }
+    // 0x06 : pinAuth
+    let pin_auth = if params.pin_auth.is_empty() {
+        None
+    } else {
+        Some(params.pin_auth.to_value())
     };
-
-    // 0x07:pinProtocol
-    let pin_protocol = Value::Integer(1);
 
     // create cbor object
     let mut get_assertion = vec![
-        (Value::Integer(0x01), rpid),
-        (Value::Integer(0x02), cdh),
+        (1.to_value(), rpid),
+        (2.to_value(), cdh),
     ];
-    if let Some(obj) = allow_list {
-        get_assertion.push((Value::Integer(0x03), obj));
+    
+    if !params.allowlist_credential_ids.is_empty() {
+        get_assertion.push((3.to_value(), allow_list));
     }
+    
+    if let Some(ext) = ext_val {
+        get_assertion.push((4.to_value(), ext));
+    }
+    
+    get_assertion.push((5.to_value(), options));
+    
+    if let Some(pin) = pin_auth {
+        get_assertion.push((6.to_value(), pin));
+        // pinProtocol(0x07)
+        get_assertion.push((7.to_value(), 1.to_value()));
+    }
+
+    common::to_payload(get_assertion, ctapdef::AUTHENTICATOR_GET_ASSERTION)
+}
+
+fn create_allow_list(credential_ids: &[Vec<u8>]) -> Value {
+    let list: Vec<Value> = credential_ids
+        .iter()
+        .map(|id| {
+            vec![
+                ("id".to_value(), id.to_value()),
+                ("type".to_value(), "public-key".to_value()),
+            ]
+            .to_value()
+        })
+        .collect();
+    list.to_value()
+}
+
+fn create_extensions(extensions: Option<&Vec<Extension>>, hmac_ext: Option<HmacExt>) -> Option<Value> {
+    let mut ext_val = Vec::new();
+
+    // HMAC Secret Extension
+    if let Some(hmac_ext) = hmac_ext {
+        let tmp = hmac_ext.shared_secret.public_key.to_value_cib().unwrap();
+        let param = vec![
+            // keyAgreement(0x01)
+            (1.to_value(), tmp),
+            // saltEnc(0x02)
+            (2.to_value(), hmac_ext.salt_enc.to_value()),
+            // saltAuth(0x03)
+            (3.to_value(), hmac_ext.salt_auth.to_value()),
+        ];
+
+        ext_val.push((
+            Extension::HmacSecret(None).to_string().to_value(),
+            param.to_value(),
+        ));
+    }
+
     if let Some(extensions) = extensions {
-        get_assertion.push((Value::Integer(0x04), extensions));
+        for ext in extensions {
+            match *ext {
+                Extension::HmacSecret(_) => (),
+                Extension::LargeBlobKey((n, _)) | Extension::CredBlob((n, _)) => {
+                    ext_val.push((ext.to_string().to_value(), n.unwrap().to_value()));
+                }
+            };
+        }
     }
-    get_assertion.push((Value::Integer(0x05), options));
-    if let Some(x) = pin_auth {
-        get_assertion.push((Value::Integer(0x06), x));
-        get_assertion.push((Value::Integer(0x07), pin_protocol));
+    
+    if ext_val.is_empty() {
+        None
+    } else {
+        Some(ext_val.to_value())
     }
-    let cbor = Value::Map(vec_to_btree_map(get_assertion));
+}
 
-    // Command - authenticatorGetAssertion (0x02)
-    let mut payload = [ctapdef::AUTHENTICATOR_GET_ASSERTION].to_vec();
-    payload.append(&mut to_vec(&cbor).unwrap());
-
-    payload
+fn create_options(up: bool, uv: Option<bool>) -> Value {
+    let mut options = vec![("up".to_value(), up.to_value())];
+    if let Some(v) = uv {
+        options.push(("uv".to_value(), v.to_value()));
+    }
+    options.to_value()
 }
