5cf7f055bfb9f3e53fc0fafc5316418fab03a85a
gebo <35388172+gebogebogebo@users.noreply.github.com>
2025-04-02 17:17:42 +0900

    Update authenticator_config_command.rs

--- src/fidokey/authenticator_config/authenticator_config_command.rs
+++ src/fidokey/authenticator_config/authenticator_config_command.rs
@@ -17,6 +17,7 @@ pub enum SubCommand {
     #[strum(props(SubCommandId = "3"))]
     ForceChangePin,
 }
+
 impl SubCommandBase for SubCommand {
     fn has_param(&self) -> bool {
         matches!(
@@ -29,72 +30,83 @@ impl SubCommandBase for SubCommand {
 }
 
 pub fn create_payload(pin_token: pintoken::PinToken, sub_command: SubCommand) -> Result<Vec<u8>> {
-    // create cbor
-    let mut map = Vec::new();
-
     // 0x01: subCommand
-    let key1: i32 = 1;
-    let sub_cmd_id: i32 = sub_command.id()? as i32;
-    map.push((key1.to_value(), sub_cmd_id.to_value()));
+    let sub_cmd_id = sub_command.id()? as i32;
+    
+    // 0x02: subCommandParams (必要な場合のみ)
+    let sub_command_params = create_sub_command_params(&sub_command)?;
+    
+    // 0x04: pinUvAuthParam
+    let pin_uv_auth_param = create_pin_uv_auth_param(&pin_token, &sub_command, &sub_command_params.1)?;
 
-    // subCommandParams (0x02): Map containing following parameters
-    let mut sub_command_params_cbor = Vec::new();
-    if sub_command.has_param() {
-        let param_vec = match sub_command.clone() {
-            SubCommand::SetMinPinLength(new_min_pin_length) => {
-                // 0x01:newMinPINLength
-                let key1: i32 = 1;
-                vec![(key1.to_value(), new_min_pin_length.to_value())]
-            }
-            SubCommand::SetMinPinLengthRpIds(rpids) => {
-                // 0x02:minPinLengthRPIDs
-                let key2: i32 = 2;
-                // RPIDsの配列を作成
-                let rpids_values: Vec<Value> = rpids.iter().map(|id| id.to_value()).collect();
-                vec![(key2.to_value(), rpids_values.to_value())]
-            }
-            SubCommand::ForceChangePin => {
-                // 0x03:ForceChangePin
-                let key3: i32 = 3;
-                vec![(key3.to_value(), true.to_value())]
-            }
-            _ => vec![],
-        };
-
-        if !param_vec.is_empty() {
-            let key2: i32 = 2;
-            let param_map = param_vec.to_value();
-            map.push((key2.to_value(), param_map.clone()));
-            
-            // シリアライズ
-            let mut cbor_data = Vec::new();
-            ciborium::ser::into_writer(&param_map, &mut cbor_data)?;
-            sub_command_params_cbor = cbor_data;
-        }
+    // CBORマップの作成
+    let mut auth_config = vec![
+        (0x01.to_value(), sub_cmd_id.to_value()),
+        (0x03.to_value(), 1.to_value()),  // pinProtocol は常に1
+        (0x04.to_value(), pin_uv_auth_param.to_value()),
+    ];
+    
+    // サブコマンドパラメータがある場合のみ追加
+    if let Some(param_map) = sub_command_params.0 {
+        auth_config.push((0x02.to_value(), param_map));
     }
 
-    // 0x03: pinProtocol
-    let key3: i32 = 3;
-    let pin_protocol: i32 = 1;
-    map.push((key3.to_value(), pin_protocol.to_value()));
+    // ペイロードの生成
+    common::to_payload(auth_config, ctapdef::AUTHENTICATOR_CONFIG)
+}
 
-    // 0x04: pinUvAuthParam
-    let pin_uv_auth_param = {
-        // pinUvAuthParam (0x04)
-        // - authenticate(pinUvAuthToken, 32×0xff || 0x0d || uint8(subCommand) || subCommandParams).
-        // https://fidoalliance.org/specs/fido-v2.1-ps-20210615/fido-client-to-authenticator-protocol-v2.1-ps-20210615.html#authenticatorConfig
-        let mut message = vec![0xff; 32];
-        message.append(&mut vec![0x0d]);
-        message.append(&mut vec![sub_command.id()?]);
-        message.append(&mut sub_command_params_cbor);
-
-        let sig = enc_hmac_sha_256::authenticate(&pin_token.key, &message);
-        sig[0..16].to_vec()
-    };
+/// サブコマンドパラメータの生成
+/// 
+/// 戻り値: (CBORマップ値（任意）, そのシリアライズされたバイト列)
+fn create_sub_command_params(sub_command: &SubCommand) -> Result<(Option<Value>, Vec<u8>)> {
+    if !sub_command.has_param() {
+        return Ok((None, Vec::new()));
+    }
     
-    let key4: i32 = 4;
-    map.push((key4.to_value(), pin_uv_auth_param.to_value()));
+    let param_vec = match sub_command {
+        SubCommand::SetMinPinLength(new_min_pin_length) => {
+            // 0x01:newMinPINLength
+            vec![(0x01.to_value(), new_min_pin_length.to_value())]
+        }
+        SubCommand::SetMinPinLengthRpIds(rpids) => {
+            // 0x02:minPinLengthRPIDs
+            let rpids_values: Vec<Value> = rpids.iter().map(|id| id.to_value()).collect();
+            vec![(0x02.to_value(), rpids_values.to_value())]
+        }
+        SubCommand::ForceChangePin => {
+            // 0x03:ForceChangePin
+            vec![(0x03.to_value(), true.to_value())]
+        }
+        _ => vec![],
+    };
 
-    // common::to_payload関数を使ってペイロードを生成
-    common::to_payload(map, ctapdef::AUTHENTICATOR_CONFIG)
+    if param_vec.is_empty() {
+        return Ok((None, Vec::new()));
+    }
+
+    let param_map = param_vec.to_value();
+    
+    // シリアライズしてバイト列を取得
+    let mut cbor_data = Vec::new();
+    ciborium::ser::into_writer(&param_map, &mut cbor_data)?;
+    
+    Ok((Some(param_map), cbor_data))
+}
+
+/// PINトークンと認証用パラメータからPIN/UV認証パラメータを作成
+fn create_pin_uv_auth_param(
+    pin_token: &pintoken::PinToken, 
+    sub_command: &SubCommand,
+    sub_command_params_cbor: &[u8]
+) -> Result<Vec<u8>> {
+    // pinUvAuthParam (0x04)
+    // - authenticate(pinUvAuthToken, 32×0xff || 0x0d || uint8(subCommand) || subCommandParams).
+    // https://fidoalliance.org/specs/fido-v2.1-ps-20210615/fido-client-to-authenticator-protocol-v2.1-ps-20210615.html#authenticatorConfig
+    let mut message = vec![0xff; 32];
+    message.append(&mut vec![0x0d]);
+    message.append(&mut vec![sub_command.id()?]);
+    message.append(&mut sub_command_params_cbor.to_vec());
+
+    let sig = enc_hmac_sha_256::authenticate(&pin_token.key, &message);
+    Ok(sig[0..16].to_vec())
 }
